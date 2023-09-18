cd8add79e489c188dfa560a684de7e61464113d1
dhangarritika <dhangarritika90@gmail.com>
2023-09-18 12:17:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import InputItems from "./components/InputItems";
+import ShowItems from "./components/ShowItems";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <InputItems />
+      <ShowItems />
+    </>
   );
 }
 
